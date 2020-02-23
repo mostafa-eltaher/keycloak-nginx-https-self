a2b7bb8bf263eb06a0e5a3441150827bccd653c0
mostafa-eltaher <mostafa.mahmoud.eltaher@gmail.com>
2020-02-23 23:29:29 +0000

    Make create_ca and issue_cert scripts are parameterized

--- create_ca.sh
+++ create_ca.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-
 set -e
 
-openssl genrsa -out rootCA.key 4096
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.crt
-openssl genrsa -out localhost.key 2048
+ca_common_name=$1
+[ -z $ca_common_name ] && ca_common_name=rootCA
+
+cat << EOF > $ca_common_name.cnf
+[ req ]
+prompt=no
+distinguished_name = req_distinguished_name
+attributes         = req_attributes
+
+[ req_distinguished_name ]
+commonName = $ca_common_name
+
+[ req_attributes ]
+req_extensions     = v3_req
+
+[ v3_req ]
+# Extensions to add to a certificate request
+basicConstraints = critical,CA:TRUE, pathlen:0
+keyUsage         = critical, cRLSign, digitalSignature, keyCertSign
+EOF
+openssl genrsa -out $ca_common_name.key 4096
+openssl req -x509 -new -nodes -config $ca_common_name.cnf -extensions v3_req -key $ca_common_name.key -sha512 -days 1827 -out $ca_common_name.crt
+
--- issue_cert.sh
+++ issue_cert.sh
@@ -1,7 +1,34 @@
 #!/bin/bash 
-
 set -e
 
-openssl genrsa -out cert.key 2048
-openssl req -config ./ssl.conf -new -key cert.key -out cert.csr
-openssl x509 -req -in cert.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out cert.crt -days 500 -sha256 -extensions v3_req -extfile ssl.conf
+cert_cn=$1
+[ -z $cert_cn ] && cert_cn=cert
+
+ca_cn=$2
+[ -z $ca_cn ] && cert_cn=rootCA
+
+cat << EOF > $cert_cn.cnf
+[ req ]
+prompt=no
+distinguished_name  = req_distinguished_name
+attributes          = req_attributes
+req_extensions      = v3_req
+
+[ req_distinguished_name ]
+commonName = $cert_cn
+
+[ req_attributes ]
+
+
+[ v3_req ]
+subjectAltName = @alt_names
+# Extensions to add to a certificate request
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+[alt_names]
+IP.1 = 10.0.2.2
+EOF
+
+openssl genrsa -out $cert_cn.key 2048
+openssl req -config $cert_cn.cnf -new -key $cert_cn.key -out $cert_cn.csr
+openssl x509 -req -in $cert_cn.csr -CA $ca_cn.crt -CAkey $ca_cn.key -CAcreateserial -out $cert_cn.crt -days 731 -sha256 -extensions v3_req -extfile $cert_cn.cnf
